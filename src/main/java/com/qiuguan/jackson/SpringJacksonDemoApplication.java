541d706ff66e6a287b7e7d515fddd0122a7b86b9
fuyuanhui <Fuyuanhuiabcd@123>
2024-07-25 00:29:30 +0800

    first commit

---
+++ src/main/java/com/qiuguan/jackson/SpringJacksonDemoApplication.java
@@ -0,0 +1,16 @@
+package com.qiuguan.jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fu yuan hui
+ */
+@SpringBootApplication
+public class SpringJacksonDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJacksonDemoApplication.class, args);
+    }
+
+}
